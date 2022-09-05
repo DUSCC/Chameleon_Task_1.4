12b9c70349d38d2e5f36680151debf3e527c6f9a
Jack Wright <jack.louie.wright@gmail.com>
2022-09-05 19:40:19 +0100

    minimal node setup, assume they already have the network set up

--- node_setup.py
+++ node_setup.py
@@ -11,9 +11,27 @@ chi.lease.add_node_reservation(head_node, resource_properties=["==", "$node_name
 head_lease = chi.lease.create_lease("CD_Head_Lease", reservations=head_node)
 head_ready = chi.lease.wait_for_active(head_lease['id'])
 
-# compute node lease
+# compute node lease by node_name
 compute_nodes = []
-chi.lease.add_node_reservation(compute_nodes, count=2, node_type="indyscc_compute")
+chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a443"])
+chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a447"])
 compute_lease = chi.lease.create_lease("CD_Compute_Lease", reservation=compute_nodes)
 compute_ready = chi.lease.wait_for_active(compute_lease['id'])
 
+# ASSUME THE ISOLATED NETWORK AND ROUTER HAVE BEEN SET UP IN THE GUI
+head = chi.server.create_server(
+    "CD_Head",
+    reservation_id=chi.lease.get_node_reservation(head_lease['id']),
+    image_name='CC-Ubuntu20.04',
+    network_name='stbl'
+)
+
+compute = chi.server.create_server(
+    "CD_Compute",
+    reservation_id=chi.lease.get_node_reservation(compute_lease['id']),
+    image_name='CC-Ubuntu20.04',
+    network_name='stbl'
+)
+
+serv_start = chi.server.wait_for_active(head_node['id'])
+chi.server.associate_floating_ip(head_node['id'])

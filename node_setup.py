2563a773b8c7a755a4b5dce973a5e1ada4b82788
Crambor <dariuscpt@gmail.com>
2022-09-09 13:17:31 +0100

    refactor: try to get network first and other fixes

--- node_setup.py
+++ node_setup.py
@@ -1,47 +1,84 @@
 #!/usr/bin/python3
 
-import chi, chi.lease, chi.server, chi.network
+import chi.lease
+import chi.server
+import chi.network
 
 chi.set('project_name', 'CHI-221014')
 chi.use_site('CHI@Purdue')
 
-# set up the network outside the ClusDur project
-stbl_net = chi.network.create_network("stbl")
-subnet = chi.network.create_subnet("private-subnet-stbl-0", chi.network.get_network_id("stbl"))
-router = chi.network.create_router("public_router")
-chi.network.add_subnet_to_router(chi.network.get_router_id("public_router"), chi.network.get_subnet_id("private-subnet-stbl-0"))
+net_name = "stbl"
+subnet_name = "private-subnet-stbl-0"
+router_name = "public_router"
 
-# head node lease by node_name
+# automatically set up the network
+try:
+    network = chi.network.get_network(net_name)
+except RuntimeError:
+    network = chi.network.create_network(net_name)
+
+try:
+    subnet = chi.network.get_subnet(subnet_name)
+except RuntimeError:
+    subnet = chi.network.create_subnet(subnet_name, network['id'], cidr="10.25.32.0/24")
+
+try:
+    router = chi.network.get_router(router_name)
+except RuntimeError:
+    # may get the error: "IpAddressGenerationFailureClient: No more IP addresses available on network"
+    router = chi.network.create_router(router_name, "public")
+    chi.network.add_subnet_to_router(router['id'], subnet['id'])
+
+
+# head node lease
 head_node = []
-chi.lease.add_node_reservation(head_node, resource_properties=["==", "$node_name", "snyder-a003"], count=1)
+chi.lease.add_node_reservation(head_node, resource_properties=["==", "$node_name", "snyder-a001"], count=1)
 head_lease = chi.lease.create_lease("CD_Head_Lease", reservations=head_node)
+
+# if the specified node is reserved, lease any node
+if head_lease is None:
+    head_node = []
+    chi.lease.add_node_reservation(head_node, node_type='indyscc_head', count=1)
+    head_lease = chi.lease.create_lease("CD_Head_Lease", reservations=head_node)
+
 head_ready = chi.lease.wait_for_active(head_lease['id'])
 
-# compute node lease by node_name
+
+# compute node lease
 compute_nodes = []
-chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a443"])
-chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a447"])
-compute_lease = chi.lease.create_lease("CD_Compute_Lease", reservation=compute_nodes)
+chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a470"])
+chi.lease.add_node_reservation(compute_nodes, count=1, resource_properties=["==", "$node_name", "rice-a330"])
+compute_lease = chi.lease.create_lease("CD_Compute_Lease", reservations=compute_nodes)
+
+# if any of the specified nodes are reserved, lease any nodes
+if compute_lease is None:
+    compute_nodes = []
+    chi.lease.add_node_reservation(compute_nodes, count=2, node_type='indyscc_compute')
+    compute_lease = chi.lease.create_lease("CD_Compute_Lease", reservations=compute_nodes)
+
 compute_ready = chi.lease.wait_for_active(compute_lease['id'])
 
-# outside the ClusDur project replace the image_name and key_name with a stock image and a generated keypair respectively.
+# Project-specific default image. Replace with stock image if not run in our project.
+head_image = 'indyscc_head-node'
+compute_image = 'indyscc-compute-node'
+ssh_key = 'Test_ssh'  # replace with your generated keypair
 
 head = chi.server.create_server(
     "CD_Head",
     reservation_id=chi.lease.get_node_reservation(head_lease['id']),
-    image_name='indyscc_head-node',
-    network_name='stbl',
-    key_name="Test_ssh"
+    image_name=head_image,
+    network_name=net_name,
+    key_name=ssh_key
 )
 
 compute = chi.server.create_server(
     "CD_Compute",
     reservation_id=chi.lease.get_node_reservation(compute_lease['id']),
-    image_name='indyscc_head-node',
-    network_name='stbl',
-    key_name="Test_ssh"
+    image_name=compute_image,
+    network_name=net_name,
+    key_name=ssh_key
 )
 
-hserv_start = chi.server.wait_for_active(head.id)
-cserv_start = chi.server.wait_for_active(compute.id)
+head_node_start = chi.server.wait_for_active(head.id)
+compute_nodes_start = chi.server.wait_for_active(compute.id)
 chi.server.associate_floating_ip(head.id)
